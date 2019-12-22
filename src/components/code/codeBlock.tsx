811d0e087eef91a42fbdab68fd6a0d39bb81271d
Yusinto Ngadiman <yusinto@gmail.com>
2019-12-21 17:04:26 -0800

    Ignore eslint exhaustive deps. FIxed snippet => snippetId. Added warning when snippetId is not set. (#44)

--- src/components/code/codeBlock.tsx
+++ src/components/code/codeBlock.tsx
@@ -8,9 +8,14 @@ export type CodeBlockProps = { snippetId: string; children: string; className: s
 export function CodeBlock({ snippetId, children, className = '' }: CodeBlockProps): ReactElement {
   const [language] = className.replace(/language-/, '').split(' ') as LanguageName[]
   const { snippets, setLanguage, unsetLanguage } = useCodeSnippetContext()
-  const languages = snippets[snippetId]
 
   useEffect(() => {
+    if (!snippetId) {
+      console.warn('You need to specify a snippetId when using CodeBlock')
+      return
+    }
+    const languages = snippets[snippetId]
+
     // Avoid duplicates
     if (languages && languages.find(({ language: l }) => l === language)) {
       return
@@ -21,7 +26,7 @@ export function CodeBlock({ snippetId, children, className = '' }: CodeBlockProp
     return () => {
       unsetLanguage(snippetId, language)
     }
-  }, [])
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return null
 }
