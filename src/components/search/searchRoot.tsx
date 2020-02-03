b9179d46d9b0163769404e63569a3bca62b59966
Yusinto Ngadiman <yusinto@gmail.com>
2020-02-03 10:36:43 -0800

    [ch63481] Add clear search text button (#133)

--- src/components/search/searchRoot.tsx
+++ src/components/search/searchRoot.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { Fragment, FunctionComponent } from 'react'
+import { Fragment, FunctionComponent, useRef } from 'react'
 import { InstantSearch, connectSearchBox } from 'react-instantsearch-dom'
 import searchClient from './searchClient'
 import Results from './results'
@@ -11,27 +11,48 @@ type SearchInputProps = {
   refine(input: string): void
 }
 
-const SearchInput: FunctionComponent<SearchInputProps> = ({ refine, ...rest }) => (
-  <Fragment>
-    <Icon name="search" variant="search" sx={{ display: ['none', 'none', 'block'] }} />
-    <input
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-      onChange={event => refine(event.currentTarget.value)}
-      {...rest}
-      sx={{
-        pl: 2,
-        border: 0,
-        borderRadius: 1,
-        height: 4,
-        fontSize: 4,
-        width: '100%',
-        '::placeholder': { fontSize: [4, 3, 4] },
-      }}
-    />
-  </Fragment>
-)
+const SearchInput: FunctionComponent<SearchInputProps> = ({ refine, ...rest }) => {
+  const inputRef = useRef(null)
+  const onClearSearch = () => {
+    inputRef.current.value = ''
+    refine('')
+  }
+
+  return (
+    <Fragment>
+      <div
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          background: 'white',
+          border: 0,
+          borderRadius: 1,
+          height: 4,
+          width: '100%',
+        }}
+      >
+        <Icon name="search" variant="search" sx={{ fill: 'graySafe' }} />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={event => refine(event.currentTarget.value)}
+          sx={{
+            border: 0,
+            width: '85%',
+            outline: 'none',
+            fontSize: 4,
+            '::placeholder': { fontSize: [4, 3, 4] },
+          }}
+          {...rest}
+        />
+        <Icon name="window-close" variant="close" onClick={onClearSearch} sx={{ cursor: 'pointer' }} />
+      </div>
+    </Fragment>
+  )
+}
 const ConnectedSearchInput = connectSearchBox(SearchInput)
 
 const Root = () => (
