acee4e2f45c961127766e814d406c38d243d5c4d
Rich Manalang <rmanalan@users.noreply.github.com>
2020-02-26 14:41:45 -0800

    [ch64926] Splunk integration docs (#191)

--- src/components/mdx/figure.tsx
+++ src/components/mdx/figure.tsx
@@ -3,5 +3,13 @@ import { jsx } from 'theme-ui'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default function Figure(props: any) {
-  return <figure {...props} sx={{ img: { variant: 'cards.image' } }} />
+  return (
+    <figure
+      {...props}
+      sx={{
+        img: { variant: 'cards.image' },
+        my: 4,
+      }}
+    />
+  )
 }
