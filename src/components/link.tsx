28e6f7c094a0f6acf49717a8ec8f05cc75c77f2c
Yusinto Ngadiman <yusinto@gmail.com>
2020-09-16 12:51:47 -0700

    [ch79926] Fix staging internal links (#372)
    * Update link.tsx
    
    * Update link.tsx

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,14 +1,24 @@
 /** @jsx jsx */
 import { jsx, Link as ThemeUILink, LinkProps } from 'theme-ui'
 import { FunctionComponent, RefAttributes, PropsWithoutRef } from 'react'
-import { Link as GatsbyLink, GatsbyLinkProps } from 'gatsby'
+import { Link as GatsbyLink, GatsbyLinkProps, useStaticQuery, graphql } from 'gatsby'
 import isExternalLink from '../utils/isExternalLink'
 
 type ForwardRef<T, P> = React.ForwardRefExoticComponent<PropsWithoutRef<P> & RefAttributes<T>>
 const ThemedGatsbyLink: ForwardRef<HTMLAnchorElement, LinkProps & GatsbyLinkProps<{}>> = ThemeUILink
 
 const Link: FunctionComponent<LinkProps & GatsbyLinkProps<{}>> = ({ to, href, ...props }) => {
-  const url = to ?? href
+  const {
+    site: { pathPrefix },
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        pathPrefix
+      }
+    }
+  `)
+
+  let url = to ?? href
   const isExternal = isExternalLink(url)
   const isMailTo = url.startsWith('mailto:')
   const isSection = url.startsWith('#')
@@ -20,6 +30,11 @@ const Link: FunctionComponent<LinkProps & GatsbyLinkProps<{}>> = ({ to, href, ..
 
   if (isExternal || isMailTo || isImage) {
     return <ThemeUILink href={url} {...props} target="_blank" rel="noopener noreferrer" />
+  }
+
+  // if path prefix is set, remove it because Gatsby Link adds it too
+  if (pathPrefix !== '' && pathPrefix !== '/') {
+    url = url.replace(pathPrefix, '')
   }
 
   return <ThemedGatsbyLink as={GatsbyLink} to={url} {...props} />
