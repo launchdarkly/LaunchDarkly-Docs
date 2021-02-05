3e496dd13aa6d70eae10acabbe7b9a1c8590d662
Charlie Park <charlie@charliepark.org>
2021-02-05 10:35:29 -0800

    Reintroduce LCP logging for Docs' first page load (#455)
    Co-authored-by: Charlie Park <cpark@launchdarkly.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,16 @@
 const { TrackJS } = require('trackjs')
-const { initDatadogRum } = require('./src/utils/browserMetricsUtils')
+const { initDataDogLogging } = require('./src/utils/browserMetricsUtils')
 
 exports.onClientEntry = () => {
   const activeEnv = process.env.GATSBY_ACTIVE_ENV
 
   // only track production and staging
   if (typeof activeEnv !== 'undefined') {
-    if (activeEnv === 'development' && process.env.RUN_DATADOG_RUM_LOCALLY === 'true') {
-      initDatadogRum()
+    if (activeEnv === 'development' && process.env.RUN_DATADOG_LOCALLY === 'true') {
+      initDataDogLogging()
     }
     if (activeEnv === 'production' || activeEnv === 'staging') {
-      initDatadogRum()
+      initDataDogLogging()
       TrackJS.install({
         token: process.env.GATSBY_TRACKJS_TOKEN,
         application: activeEnv === 'production' ? 'docs-production' : 'docs-staging',
--- src/utils/browserMetricsUtils.ts
+++ src/utils/browserMetricsUtils.ts
@@ -1,6 +1,10 @@
 import { datadogRum } from '@datadog/browser-rum'
+import { datadogLogs } from '@datadog/browser-logs'
+import { getLCP, Metric } from 'web-vitals'
 
-export const initDatadogRum = () => {
+const LCP_SLO_TARGET = 4000
+
+const initDatadogRum = () => {
   datadogRum.init({
     applicationId: process.env.GATSBY_DATADOG_RUM_APPLICATION_ID,
     clientToken: process.env.GATSBY_DATADOG_RUM_CLIENT_TOKEN,
@@ -10,4 +14,33 @@ export const initDatadogRum = () => {
     sampleRate: 100,
     trackInteractions: true,
   })
+}
+
+const sendLCPToDatadog = (metric: Metric) => {
+  const sloLogger = datadogLogs.getLogger('sloLogger')
+  const withinSlo = metric.value <= LCP_SLO_TARGET
+  const logMessage = {
+    custom: {
+      measures: {
+        ['largest_contentful_paint']: metric.value,
+        ['largest_contentful_paint_within_slo']: withinSlo,
+      },
+    },
+  }
+  sloLogger.info('LCP recorded from docs', logMessage)
+}
+
+const logLCP = () => {
+  datadogLogs.init({
+    clientToken: process.env.GATSBY_DATADOG_CLIENT_TOKEN,
+    env: process.env.GATSBY_ACTIVE_ENV,
+  })
+  datadogLogs.addLoggerGlobalContext('service', 'docs')
+  datadogLogs.createLogger('sloLogger')
+  getLCP(metric => sendLCPToDatadog(metric))
+}
+
+export const initDataDogLogging = () => {
+  initDatadogRum()
+  logLCP()
 }
