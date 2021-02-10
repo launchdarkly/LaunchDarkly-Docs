d6389b70c6b821be82b46fb8687c5a8aba0ffcd0
Charlie Park <charlie@charliepark.org>
2021-02-10 14:16:05 -0800

    Limit Datadog logging to the Production environment (#469)
    Co-authored-by: Charlie Park <cpark@launchdarkly.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,11 @@ const { initDataDogLogging } = require('./src/utils/browserMetricsUtils')
 exports.onClientEntry = () => {
   const activeEnv = process.env.GATSBY_ACTIVE_ENV
 
-  // only track production and staging
   if (typeof activeEnv !== 'undefined') {
-    if (activeEnv === 'development' && process.env.RUN_DATADOG_LOCALLY === 'true') {
+    if (activeEnv === 'production' || (activeEnv === 'development' && process.env.RUN_DATADOG_LOCALLY === 'true')) {
       initDataDogLogging()
     }
     if (activeEnv === 'production' || activeEnv === 'staging') {
-      initDataDogLogging()
       TrackJS.install({
         token: process.env.GATSBY_TRACKJS_TOKEN,
         application: activeEnv === 'production' ? 'docs-production' : 'docs-staging',
