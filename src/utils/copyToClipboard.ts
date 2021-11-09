436c5bbad1abfb7f3bd51979703fe24c1b98474e
Jaz White <jwhite@launchdarkly.com>
2021-11-09 12:37:25 -0500

    [sc-130556] update copy to clipboard (#945)
    - remove use of deprecated copy function
      - See: https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
    - unify copy to clipboard fxn

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,30 +1,3 @@
-type CopyToClipboardOptions = {
-  preserveFormatting?: boolean
-}
-
-export function copyToClipboard(s: string, { preserveFormatting = false }: CopyToClipboardOptions = {}) {
-  const span = document.createElement(preserveFormatting ? 'pre' : 'span')
-  span.textContent = s
-  document.body.append(span)
-
-  const selection = window.getSelection()
-  const range = window.document.createRange()
-
-  // TODO: fallback?
-  if (!selection) {
-    return
-  }
-
-  selection.removeAllRanges()
-  range.selectNodeContents(span)
-  selection.addRange(range)
-
-  try {
-    window.document.execCommand('copy')
-  } catch (e) {
-    // Give up
-  }
-
-  selection.removeAllRanges()
-  window.document.body.removeChild(span)
+export function copyToClipboard(s: string) {
+  navigator.clipboard.writeText(s)
 }
