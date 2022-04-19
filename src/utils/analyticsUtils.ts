d19e62ab9fed210342c82cf6d65170e4a495214d
Yusinto Ngadiman <yusinto@gmail.com>
2022-04-19 09:54:31 -1000

    [sc-150530] Fix ga search events bug (#1645)
    * Update searchRoot.tsx
    
    * Update searchRoot.tsx
    
    * Rollback firefox fix and just fix search term bug.

--- src/utils/analyticsUtils.ts
+++ src/utils/analyticsUtils.ts
@@ -1,14 +1,15 @@
 import debounce from 'lodash.debounce'
+import { SearchState } from 'react-instantsearch-core'
 
 // Track custom events and sends them to segment.io e.g. track('Link to API docs clicked')
 export const track = (event: string) => window?.analytics?.track?.(event)
 
 // Sends search events directly to gtm without segment.io.
 // GOTCHA: this is private! Use the debounced export instead.
-const _trackSearch = (searchTerm: string) => {
+const _trackSearch = (searchState: SearchState) => {
   // Follow the gtags api for search related events:
   // https://developers.google.com/tag-platform/gtagjs/reference#event
-  window?.gtag?.('event', 'search', { search_term: searchTerm })
+  window?.gtag?.('event', 'search', { search_term: searchState.query })
 }
 
 // Debounce 500ms using lodash to prevent each search keystroke spamming gtm
