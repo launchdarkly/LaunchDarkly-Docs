33e49d1a456321b9813381cb03dfc5c05f04c91d
Kiara <kcacosta@launchdarkly.com>
2023-01-23 13:42:51 -0500

    feat: [sc-184331] Add optional "site" property to provide visibility for federal customers (#2658)
    add logic for site choice ld or fed

--- src/components/mdx/code/code2abItem.tsx
+++ src/components/mdx/code/code2abItem.tsx
@@ -1,9 +1,31 @@
-import React, { PropsWithChildren } from 'react'
+import React, { Children, PropsWithChildren, ReactElement } from 'react'
+import qs from 'qs'
+
+import { errorOnInvalidSite } from '../../../utils/siteAwareUtils'
+import useSite from '../../siteSelector/useSite'
 
 export type TabsItemProps2 = {
   label: string
 }
 
 export function CodeTabItem2({ children }: PropsWithChildren<TabsItemProps2>) {
-  return <>{children}</>
+  const [site] = useSite()
+  const childrenArray = Children.toArray(children)
+
+  let defaultChild: ReactElement
+  const siteAwareItem = childrenArray.find((child: ReactElement) => {
+    const metaString = child.props?.children?.props?.metastring
+    const { site: codeTabItemSite } = qs.parse(metaString, { ignoreQueryPrefix: true })
+
+    errorOnInvalidSite(codeTabItemSite as string)
+
+    // the default codeTabItem is the first that does not have a site prop
+    if (!defaultChild && !codeTabItemSite) {
+      defaultChild = child
+    }
+
+    return codeTabItemSite === site
+  })
+
+  return <>{siteAwareItem ?? defaultChild}</>
 }
