3b09f448b6cf637aad8c6fdc1de506dbd8ce233e
Kiara <kcacosta@launchdarkly.com>
2023-03-17 09:53:37 -0400

    feat(pd - documentation): [sc-190020] feature request: add a check to the CodeSample component -- error if no CSTab elements (#2935)
    * added error handling for case when child component is missing
    
    * throw error if child component is missing
    
    * error message suggestions
    
    * function name

--- src/components/mdx/code/codeSample.tsx
+++ src/components/mdx/code/codeSample.tsx
@@ -10,46 +10,51 @@ type CodeSampleProps = {
 
 export function CodeSample({ children }: CodeSampleProps) {
   const [selectedIndex, setSelectedIndex] = useState<number>(0)
-  const validChildren = Children.toArray(children).filter((child: { props: { mdxType?: string } }) => {
-    return child.props.mdxType === 'CSTab'
-  })
-  const selectedChild = validChildren.find((_child, index) => {
-    return index === selectedIndex
-  })
-  validChildren.forEach((CSTabItem: { props: CSTabProps }, index) => {
-    throwErrorIfLabelMissing(CSTabItem, index)
-  })
-  return (
-    <Card variant="code">
-      <Flex sx={{ flexDirection: 'column' }}>
-        <Box sx={{ marginBottom: 4 }}>
-          {validChildren.map((child: { props: { label: string } }, index) => {
-            const { label } = child.props
-            return (
-              <Button
-                aria-label="label"
-                variant={`code.${index === selectedIndex ? 'languageActive' : 'language'}`}
-                key={index}
-                onClick={() => setSelectedIndex(index)}
-                title={label}
-              >
-                <div
-                  sx={{
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                  }}
+  if (!children) {
+    throw new Error(`Error: There is a ${CodeSample.name} that is missing a child component. Add a CSTab.`)
+  } else {
+    const validChildren = Children.toArray(children).filter((child: { props: { mdxType?: string } }) => {
+      return child.props.mdxType === 'CSTab'
+    })
+    const selectedChild = validChildren.find((_child, index: number) => {
+      return index === selectedIndex
+    })
+    console.log('validChildren', validChildren)
+    validChildren.forEach((CSTabItem: { props: CSTabProps }, index: number) => {
+      throwErrorIfLabelMissing(CSTabItem, index)
+    })
+    return (
+      <Card variant="code">
+        <Flex sx={{ flexDirection: 'column' }}>
+          <Box sx={{ marginBottom: 4 }}>
+            {validChildren.map((child: { props: { label: string } }, index: number) => {
+              const { label } = child.props
+              return (
+                <Button
+                  aria-label="label"
+                  variant={`code.${index === selectedIndex ? 'languageActive' : 'language'}`}
+                  key={index}
+                  onClick={() => setSelectedIndex(index)}
+                  title={label}
                 >
-                  {label}
-                </div>
-              </Button>
-            )
-          })}
-        </Box>
-        {selectedChild}
-      </Flex>
-    </Card>
-  )
+                  <div
+                    sx={{
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                      overflow: 'hidden',
+                    }}
+                  >
+                    {label}
+                  </div>
+                </Button>
+              )
+            })}
+          </Box>
+          {selectedChild}
+        </Flex>
+      </Card>
+    )
+  }
 }
 
 function throwErrorIfLabelMissing(CSTabItem: { props: CSTabProps }, index: number) {
