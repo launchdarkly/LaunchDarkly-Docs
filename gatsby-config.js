787c3a38f81da3af979926800407c0c86cab203f
Ember Stevens <79482775+ember-stevens@users.noreply.github.com>
2023-04-25 15:40:38 -0700

    Attribute filters are now fully GA--remove "You may not be able to use attribute filters" callout (#3057)
    * Removes callout
    
    * Fix linter error

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,8 +124,6 @@ const plugins = [
     },
   },
   {
-    // Google tag manager was requested by MOps. They are working with a vendor to set up site tagging to give us more
-    // granular engagement tracking. sc-188749
     resolve: 'gatsby-plugin-google-tagmanager',
     options: {
       id: 'GTM-M4F7FSC',
