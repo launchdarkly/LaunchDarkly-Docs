c8cd7ad14efa39e99d90196c05517c8611618591
Christian <cjhensen@users.noreply.github.com>
2023-05-09 10:06:15 -0400

    import component

--- src/components/flags/StaticSiteDemo.tsx
+++ src/components/flags/StaticSiteDemo.tsx
@@ -3,10 +3,21 @@
 import { jsx } from 'theme-ui'
 
 export const StaticSiteDemo = () => (
-  <div style="height:450px; width: 100%;margin-bottom:20px">
+  <div
+    style={{
+      height: '450px',
+      width: '100%',
+      marginBottom: '20px',
+    }}
+  >
     <iframe
       src="https://feature-flagging-static-sites.netlify.app/"
-      style="height: 100%; width: 100%; border:1px solid #ccc;border-radius:5px"
+      style={{
+        height: '100%',
+        width: '100%',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+      }}
       title="Feature flagging static sites demo"
     ></iframe>
   </div>
