c504f0b71d2ac1978989e377103348544e69b9de
Christian <cjhensen@users.noreply.github.com>
2023-05-10 16:09:56 -0400

    revert the state usage, and use suspense for ssr to avoid fouc

--- src/components/sideNav/desktopSideNav.tsx
+++ src/components/sideNav/desktopSideNav.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { useEffect, useState } from 'react'
+import { Suspense } from 'react'
 import { globalHistory } from '@reach/router'
 import { graphql, useStaticQuery } from 'gatsby'
 import { jsx } from 'theme-ui'
@@ -7,11 +7,8 @@ import { jsx } from 'theme-ui'
 import { findRootTopic } from '../../utils/navigationDataUtils'
 
 import TreeNode from './treeNode'
-import { SideNavItem } from './types'
 
 const DesktopSideNav = () => {
-  const [currentNode, setCurrentNode] = useState<SideNavItem>(null)
-
   const {
     site: { pathPrefix },
     allNavigationData: { nodes: navigationData },
@@ -55,32 +52,37 @@ const DesktopSideNav = () => {
     }
   `)
 
-  useEffect(() => {
-    setCurrentNode(findRootTopic(topics, navigationData, pathPrefix))
-  }, [topics, navigationData, pathPrefix])
-
-  if (!currentNode) return null
+  const currentNode = findRootTopic(topics, navigationData, pathPrefix)
+  if (!currentNode && typeof window !== 'undefined') {
+    console.error(`Can't find root topic! pathPrefix: ${pathPrefix}`)
+  }
 
   return (
-    <nav
-      sx={{
-        bg: theme => theme.colors.grayscaleGray100,
-        display: ['none', 'block'],
-        top: '4.5rem',
-        bottom: 0,
-        position: 'fixed',
-        overflow: 'auto',
-        pt: 4,
-        width: '19rem',
-      }}
-    >
-      <TreeNode
-        currentPath={
-          pathPrefix !== '' ? globalHistory.location.pathname.replace(pathPrefix, '') : globalHistory.location.pathname
-        }
-        nodes={currentNode.items}
-      />
-    </nav>
+    <Suspense fallback={null}>
+      {currentNode && (
+        <nav
+          sx={{
+            bg: theme => theme.colors.grayscaleGray100,
+            display: ['none', 'block'],
+            top: '4.5rem',
+            bottom: 0,
+            position: 'fixed',
+            overflow: 'auto',
+            pt: 4,
+            width: '19rem',
+          }}
+        >
+          <TreeNode
+            currentPath={
+              pathPrefix !== ''
+                ? globalHistory.location.pathname.replace(pathPrefix, '')
+                : globalHistory.location.pathname
+            }
+            nodes={currentNode.items}
+          />
+        </nav>
+      )}
+    </Suspense>
   )
 }
 
