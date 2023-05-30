f36edd0d69f0d5002fdb7660042f975701b091aa
Christian <cjhensen@users.noreply.github.com>
2023-05-30 11:58:24 -0400

    add gatsby-ssr with wrap page element

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import type { GatsbySSR } from 'gatsby'
+import { jsx } from 'theme-ui'
+
+import LayoutContainer from './src/layouts/layoutContainer'
+import { PageContext } from './src/types/pageContext'
+
+export const wrapPageElement: GatsbySSR<unknown, PageContext>['wrapPageElement'] = ({ element, props }) => {
+  return <LayoutContainer pageContext={props.pageContext}>{element}</LayoutContainer>
+}
