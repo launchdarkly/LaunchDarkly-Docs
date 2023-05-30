0208c89c1139c138ff98791748fbd4fc96206c06
Christian <cjhensen@users.noreply.github.com>
2023-05-30 13:14:39 -0400

    default return for lack of pageContext

--- src/layouts/layoutContainer.tsx
+++ src/layouts/layoutContainer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { Fragment, ReactNode } from 'react'
 
 import { PageContext } from '../types/pageContext'
 
@@ -7,10 +7,15 @@ import Layout from './layout'
 
 type LayoutContainerProps = {
   children: ReactNode
-  pageContext: PageContext
+  pageContext?: PageContext
 }
 
 const LayoutContainer = ({ children, pageContext }: LayoutContainerProps) => {
+  // pages not created programmatically will not have a pageContext, such as 404.tsx
+  if (!pageContext || Object.keys(pageContext).length === 0) {
+    return <Fragment>{children}</Fragment>
+  }
+
   const isInternal = pageContext?.frontmatter?.isInternal
   if (isInternal) {
     return <InternalLayout>{children}</InternalLayout>
