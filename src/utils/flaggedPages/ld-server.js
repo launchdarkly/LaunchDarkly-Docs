78dbe3b44d2ecf0b1e0bc57f2e6427d5f0f46f76
Christian <cjhensen@users.noreply.github.com>
2023-06-08 14:19:26 -0400

    update modules for flagged pages config

--- src/utils/flaggedPages/ld-server.js
+++ src/utils/flaggedPages/ld-server.js
@@ -2,9 +2,7 @@
 // must use commonJS here because of the algolia plugin in gatsby-config.js
 
 const ld = require('launchdarkly-node-server-sdk')
-const defaultConfig = require('./defaultConfig').defaultConfig
-
-const CONFIG_FLAG_KEY = 'docs-pages-config'
+const { DEFAULT_CONFIG, CONFIG_FLAG_KEY, PAGE_CONTEXT } = require('./.')
 
 const options = {
   baseUri: 'https://sdk.ld.catamorphic.com',
@@ -15,13 +13,8 @@ const options = {
   }),
 }
 
-const context = {
-  kind: 'github',
-  key: 'github',
-}
-
 const getFlaggedPagesConfig = async () => {
-  let disabledPaths = defaultConfig.disabled_paths
+  let disabledPaths = DEFAULT_CONFIG.disabled_paths
 
   const isPathDisabled = path => disabledPaths.includes(path)
 
@@ -33,7 +26,7 @@ const getFlaggedPagesConfig = async () => {
   const client = ld.init(process.env.LAUNCHDARKLY_SDK_KEY, options)
   try {
     await client.waitForInitialization()
-    const config = await client.variation(CONFIG_FLAG_KEY, context, defaultConfig)
+    const config = await client.variation(CONFIG_FLAG_KEY, PAGE_CONTEXT, DEFAULT_CONFIG)
 
     console.info('Flagged pages config', config)
 
