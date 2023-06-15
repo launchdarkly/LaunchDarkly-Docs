192e2b9211d2860d96713aa8937cb0c5e5ff9c5b
Casey Waldren <cwaldren@launchdarkly.com>
2023-06-15 09:31:27 -0700

    update endOfLife.tsx to account for releases on previous major versions (#3020)
    * update endOfLife.tsx to account for releases on previous major versions
    
    The current script would assign (next release + 1 year) EOL dates to all releases. This is undesirable in the
    scenario where we put out a minor release on a previous major version: that would cause all releases prior to it
    to have EOL dates relative to it, which would extend the support window out beyond the *current* major version.
    
    This change caps the EOL date at min(next version + 1 year, next major + 1 year). The effect is that we can put out
    minors on older major branches, and their EOL dates will have an upper bound of the major's release + 1 year.

--- src/components/endOfLife.tsx
+++ src/components/endOfLife.tsx
@@ -15,46 +15,119 @@ export function RelayEndOfLife() {
   return <EndOfLife data={relayData} expandable={false} />
 }
 
-type EOLVersion = {
+/**
+ * Represents a single version entry within an SDK's EOL policy JSON.
+ */
+type RawVersionEntry = {
   versionMajorMinor: string
   releaseYear: number
   releaseMonthDay: string
 }
 
-type EOL = {
+/**
+ * Represents a list of version entries specific to an SDK in the policy JSON.
+ */
+type RawEOLPolicy = {
   name: string
-  versions: EOLVersion[]
+  versions: RawVersionEntry[]
 }
 
+/**
+ * Represents a transformation of a raw entry in order to parse out the
+ * major/minor versions from the combined versionMajorMinor field.
+ */
+type VersionEntry = {
+  versionMajor: number
+  versionMinor: number
+  releaseYear: number
+  releaseMonthDay: string
+}
+
+/**
+ * Represents a list of VersionEntries suitable for usage in EOL calculations.
+ */
+type EOLPolicy = {
+  name: string
+  versions: VersionEntry[]
+}
+
+/**
+ * Passed into the EndOfLife components.
+ */
 type EndOfLifeProps = {
-  data: EOL[]
+  data: RawEOLPolicy[]
   expandable: boolean
 }
 
 function EndOfLife({ data, expandable }: EndOfLifeProps) {
-  const eols: EOL[] = data
+  const getVersionNumbers = (semver: string) => {
+    return semver.split('.').map(v => parseInt(v))
+  }
+
+  const parseSemVer = (eols: RawVersionEntry[]): VersionEntry[] => {
+    return eols.map(eol => {
+      const [major, minor] = getVersionNumbers(eol.versionMajorMinor)
+      return { ...eol, versionMajor: major, versionMinor: minor }
+    })
+  }
+
+  const getFullVersion = (v: VersionEntry) => {
+    return v.versionMajor + '.' + v.versionMinor
+  }
+
+  const getDisplayVersion = (v: VersionEntry) => {
+    return getFullVersion(v) + '.x'
+  }
+
+  const getInitialReleaseDate = (v: VersionEntry) => {
+    return v.releaseYear + '-' + v.releaseMonthDay
+  }
+
+  const getEOLText = (v: VersionEntry) => {
+    return v.releaseYear + 1 + '-' + v.releaseMonthDay
+  }
+
+  const getEOL = (vs: VersionEntry[], i: number) => {
+    if (i == 0) {
+      return 'Current'
+    }
+    const nextMajor = getNextMajor(vs, i)
+    const nextRelease = i - 1
+
+    // If this is a version within the currently supported major version,
+    // then the EOL date is the next release + 1 year.
+    if (nextMajor == i) {
+      return getEOLText(vs[nextRelease])
+    }
+
+    // Otherwise, there's another major version out: cap the EOL date
+    // at min(nextMajor + 1 year, nextRelease + 1 year). Otherwise, releases
+    // to old major branches will indefinitely extend the support window.
+
+    const nextMajorEOL = getEOLText(vs[nextMajor])
+    const nextReleaseEOL = getEOLText(vs[nextRelease])
+
+    if (new Date(nextMajorEOL) < new Date(nextReleaseEOL)) {
+      return nextMajorEOL
+    } else {
+      return nextReleaseEOL
+    }
+  }
+  const getNextMajor = (vs: VersionEntry[], i: number) => {
+    const currMajor = vs[i].versionMajor
+    for (let j = i - 1; j >= 0; j--) {
+      if (vs[j].versionMajor > currMajor) {
+        return j
+      }
+    }
+    return i
+  }
+
+  const eols: EOLPolicy[] = data.map(eol => ({ name: eol.name, versions: parseSemVer(eol.versions) }))
 
   if (!eols) {
     console.warn("There's no EOL data")
     return null
-  }
-
-  const getVersion = (v: EOLVersion) => {
-    return v.versionMajorMinor + '.x'
-  }
-
-  const getInitialReleaseDate = (v: EOLVersion) => {
-    return v.releaseYear + '-' + v.releaseMonthDay
-  }
-
-  const getEOLText = (v: EOLVersion) => {
-    return v.releaseYear + 1 + '-' + v.releaseMonthDay
-  }
-
-  const getEOL = (vs: EOLVersion[], i: number) => {
-    if (i == 0) return 'Current'
-    else if (vs.length > i) return getEOLText(vs[i - 1])
-    else return 'Unknown'
   }
 
   return (
@@ -73,15 +146,16 @@ function EndOfLife({ data, expandable }: EndOfLifeProps) {
             </TableHeader>
             <TableBody>
               {versions.map((v, i) => (
-                <TableRow key={name + v.versionMajorMinor}>
-                  <TableCell>{getVersion(v)}</TableCell>
+                <TableRow key={name + getFullVersion(v)}>
+                  <TableCell>{getDisplayVersion(v)}</TableCell>
                   <TableCell>{getInitialReleaseDate(v)}</TableCell>
+
                   <TableCell>{getEOL(versions, i)}</TableCell>
                 </TableRow>
               ))}
-              {oldestVersion.versionMajorMinor !== '1.0' ? (
+              {getFullVersion(oldestVersion) !== '1.0' ? (
                 <TableRow key={name + 'Older'}>
-                  <TableCell>{'< ' + getVersion(oldestVersion)}</TableCell>
+                  <TableCell>{'< ' + getDisplayVersion(oldestVersion)}</TableCell>
                   <TableCell>{'Various'}</TableCell>
                   <TableCell>{getEOLText(oldestVersion) + ' or earlier'}</TableCell>
                 </TableRow>
