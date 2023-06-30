c40a76c45c3be7f648fbfdba7491dba1f847e935
Molly <molly.jones@launchdarkly.com>
2023-06-30 13:57:50 -0700

    Add missing custom role actions (#3260)
    * add missing actions
    
    * add release pipeline actions (flagged)

--- src/flags/index.ts
+++ src/flags/index.ts
@@ -1,3 +1,5 @@
 import { flagPredicate } from './flagPredicate'
 
 export const docsDemoFlagBoolean = flagPredicate('docs-demo-flag-boolean', false)
+
+export const docsRiskFreeReleasesM1 = flagPredicate('docs-risk-free-releases-m-1', false)
