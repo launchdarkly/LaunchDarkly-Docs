530a739bf6502bc09d02570708c2f8208cfff25e
Molly <molly.jones@launchdarkly.com>
2023-07-11 10:55:45 -0700

    add application actions, behind docsEnableAppsAndVersions flag (#3282)

--- src/flags/index.ts
+++ src/flags/index.ts
@@ -3,3 +3,4 @@ import { flagPredicate } from './flagPredicate'
 export const docsDemoFlagBoolean = flagPredicate('docs-demo-flag-boolean', false)
 
 export const docsRiskFreeReleasesM1 = flagPredicate('docs-risk-free-releases-m-1', false)
+export const docsEnableAppsAndVersions = flagPredicate('docs-enable-apps-and-versions', false)
