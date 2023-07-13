24a00dd0d62240697227d6f911affd9aa0a08023
Kelly Hofmann <55991524+k3llymariee@users.noreply.github.com>
2023-07-13 16:17:43 -0700

    SCIM for azure (flagged) (#2911)
    * wip: start adding azure SCIM to docs
    
    * add azure SCIM setup instructions and screenshots
    
    * update for groups and add schema names
    
    * try escaping ampersand
    
    * add line breaks
    
    * extra line break
    
    * additional screenshots and small changes
    
    * PR feedback
    
    * add note about multiple account management
    
    * Apply suggestions from code review
    
    Co-authored-by: Molly <molly.jones@launchdarkly.com>
    Co-authored-by: Ember Stevens <79482775+ember-stevens@users.noreply.github.com>
    
    * additional PR feedback
    
    * Apply suggestions from code review
    
    * Updates TK
    
    * Adds flag to index file
    
    * Adds flags to frontmatter
    
    * Flags content
    
    * Removes false variation from Azure topic
    
    * Forcing a rebuild
    
    * Removes extra space
    
    * Adds spaces within flag tags
    
    * Flags table row instead of whole table
    
    * Adds whitespace
    
    ---------
    
    Co-authored-by: Molly <molly.jones@launchdarkly.com>
    Co-authored-by: Ember Stevens <79482775+ember-stevens@users.noreply.github.com>
    Co-authored-by: Ember Stevens <ember.stevens@launchdarkly.com>

--- src/flags/index.ts
+++ src/flags/index.ts
@@ -2,5 +2,6 @@ import { flagPredicate } from './flagPredicate'
 
 export const docsDemoFlagBoolean = flagPredicate('docs-demo-flag-boolean', false)
 
+export const docsEnableScimForAzure = flagPredicate('docs-enable-scim-for-azure', false)
 export const docsRiskFreeReleasesM1 = flagPredicate('docs-risk-free-releases-m-1', false)
 export const docsEnableAppsAndVersions = flagPredicate('docs-enable-apps-and-versions', false)
